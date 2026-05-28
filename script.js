71ae91cdb8e527faf8e1f074366db43fb106fca7
eskandersetti-arch <eskandersetti@gmail.com>
2026-05-28 10:30:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // THEME TOGGLE
-function toggleTheme(){
+function toggleTheme() {
     document.body.classList.toggle("light");
 }
 
 // SCROLL REVEAL
-function reveal(){
+function reveal() {
     const elements = document.querySelectorAll(".reveal");
 
-    elements.forEach(el=>{
+    elements.forEach(el => {
         const top = el.getBoundingClientRect().top;
-        if(top < window.innerHeight - 100){
+        if (top < window.innerHeight - 100) {
             el.classList.add("active");
         }
     });
 }
 
 window.addEventListener("scroll", reveal);
-reveal();oaded successfully 🚀");
+reveal();
